86da977573555371d47343b8bee151f2b1d1df44
rafael <ashkan.saint@gmail.com>
2025-03-27 10:46:52 +0330

    changed index2

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(bodyParser.json({limit:"30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 app.use(cors());
 app.use('/posts',postRoutes);
-app.use('/');
+app.get('/',(req,res)=>{
+  res.send('Welcome to mechanicus API, WHY ARE YOU HERE?????');
+});
 
 const PORT = process.env.PORT || 5000;
 
